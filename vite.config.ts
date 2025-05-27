52b88455a712bb1603a264c8f0c394f560136b84
Jose J. Cruz <jjzcru@gmail.com>
2025-05-27 11:53:03 -0500

    chore: Add commitizen config and setup tests
    [object Object],[object Object],[object Object],[object Object]

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  test: {
+    setupFiles: ['src/setupTests.ts'],
+    coverage: {
+      exclude: ['*.config.*', '*.d.ts'],
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
